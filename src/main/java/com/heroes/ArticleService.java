afd74b5d87910c068620c6777ad5ffa8cd4f121c
Fotache, Dragos <Dragos.Fotache@awinta.de>
2016-07-14 16:24:22 +0300

    ...

--- src/main/java/com/heroes/ArticleService.java
+++ src/main/java/com/heroes/ArticleService.java
@@ -2,6 +2,7 @@ package com.heroes;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -38,6 +39,42 @@ public enum ArticleService {
 		ps.close();
 		c.close();
 
+		return result;
+	}
+	
+	public List<Article> getArticlesSlice(int first, int rows, String sortField, boolean desc) throws SQLException, ClassNotFoundException {
+		List<Article> result = new ArrayList<Article>();
+		
+		Connection c = null;
+		Class.forName("org.sqlite.JDBC");
+		c = DriverManager.getConnection("jdbc:sqlite:test.db");
+		
+		String sqlAsc = "SELECT * FROM ARTICLE ORDER BY %s LIMIT ? OFFSET ?";
+		String sqlDesc = "SELECT * FROM ARTICLE ORDER BY %s DESC LIMIT ? OFFSET ?";
+		
+		String sql = String.format(desc ? sqlDesc : sqlAsc, sortField);
+		
+		PreparedStatement ps = c.prepareStatement(sql);
+		
+		ps.setInt(1, rows);
+		ps.setInt(2, first);
+		
+		ResultSet rs = ps.executeQuery();
+		
+		while (rs.next()) {
+			int id = rs.getInt(1);
+			String pzn = rs.getString(2);
+			String name = rs.getString(3);
+			String supplier = rs.getString(4);
+			String packing = rs.getString(5);
+			
+			result.add(new Article(id, pzn, name, supplier, packing));
+		}
+		
+		rs.close();
+		ps.close();
+		c.close();
+		
 		return result;
 	}
 }
--- src/main/java/com/heroes/RestService.java
+++ src/main/java/com/heroes/RestService.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
@@ -13,7 +14,7 @@ public class RestService {
 	@GET
 	@Produces("application/json")
 	@Path("/articles")
-	public Response getStartingPage() throws ClassNotFoundException, SQLException {
+	public Response getAllArticles() throws ClassNotFoundException, SQLException {
 		Collection<Article> output = ArticleService.INSTANCE.getArticles();
 		
 		return Response
@@ -21,5 +22,28 @@ public class RestService {
 					.header("Access-Control-Allow-Origin", "*")
 					.entity(output)
 					.build();
+	}
+	
+	@GET
+	@Produces("application/json")
+	@Path("/articles/{first}/{rows}/{sortField}/{sortOrder}")
+	public Response getArticlesSlice(@PathParam("first") int first, 
+										@PathParam("rows") int rows,
+										@PathParam("sortField") String sortFieldParam,
+										@PathParam("sortOrder") String sortOrder) throws ClassNotFoundException, SQLException {
+		
+		String sortField;
+		if ("undefined".equals(sortFieldParam)) {
+			sortField = "id";
+		} else {
+			sortField = sortFieldParam;
+		}
+		Collection<Article> output = ArticleService.INSTANCE.getArticlesSlice(first, rows, sortField, "desc".equals(sortOrder) ? true : false);
+		
+		return Response
+				.status(200)
+				.header("Access-Control-Allow-Origin", "*")
+				.entity(output)
+				.build();
 	}
 }
